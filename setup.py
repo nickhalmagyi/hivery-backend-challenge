82949bbabc6a917aba7dc62ec562c3bd9a24f31d
Nick Halmagyi <nhalmagyi@gmail.com>
2019-03-08 23:50:07 +1100

    updated setup, installing pymongo before importing from mongo_init

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import subprocess
 
-from setup.mongo_init import load_data_to_mongo
 
 if __name__ == "__main__":
 
     subprocess.run(["pip", "install", "--upgrade", "pip"])
     subprocess.run(["pip3", "install", "-r", "setup/requirements.txt"])
+
+    from setup.mongo_init import load_data_to_mongo
     load_data_to_mongo()
